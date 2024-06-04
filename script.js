e5934e117574e534d00d0105612528381a8a06cb
C043 <mariofragnitoph@gmail.com>
2024-06-04 19:39:58 +0200

    Adds "hot" badge function

--- script.js
+++ script.js
@@ -4,5 +4,19 @@ const countTrips = () => {
   alert("Scegli Eveline-R, abbiamo " + cards.length + " viaggi disponibili!");
 };
 
-// modal js
-// window.onload = countTrips;
+// funzione che mette i badge hot
+const badgeGen = () => {
+  const cards = document.querySelectorAll("#collapseSummer .card");
+  console.log(cards);
+  for (let i = 0; i < cards.length; i++) {
+    const currentCard = cards[i];
+    currentCard.classList.add("position-relative");
+    const badge = document.createElement("span");
+    badge.className = "badge rounded-pill text-bg-danger position-absolute";
+    badge.style.top = "5px";
+    badge.style.right = "5px";
+    badge.innerText = "HOT";
+    currentCard.appendChild(badge);
+  }
+};
+window.onload = badgeGen;

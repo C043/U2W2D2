e508bffea8810f9bbdc833f62b0446f8db1e5e8e
Mario Fragnito <mariofragnitoph@gmail.com>
2024-06-04 17:15:49 +0200

    Adds countTrips function

---
+++ script.js
@@ -0,0 +1,7 @@
+// funzione per contare i viaggi nella pagina e mostra un alert
+const countTrips = () => {
+  const cards = document.getElementsByClassName("card");
+  alert("Scegli Eveline-R, abbiamo " + cards.length + " viaggi disponibili!");
+};
+
+// window.onload = countTrips;
